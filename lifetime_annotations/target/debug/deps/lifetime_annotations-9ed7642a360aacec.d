ad4dff7ccebd7c6476d744a6d4ecd59cd049aab8
Harry Newton <harryjnewton98@gmail.com>
2024-01-25 13:21:04 +1000

    Re-organising file structure

--- lifetime_annotations/target/debug/deps/lifetime_annotations-9ed7642a360aacec.d
+++
@@ -1,5 +0,0 @@
-/Users/harrynewton/Desktop/Projects/crust_of_rust/lifetime_annotations/target/debug/deps/lifetime_annotations-9ed7642a360aacec: src/lib.rs
-
-/Users/harrynewton/Desktop/Projects/crust_of_rust/lifetime_annotations/target/debug/deps/lifetime_annotations-9ed7642a360aacec.d: src/lib.rs
-
-src/lib.rs:
--- lifetime_annotations/target/debug/deps/lifetime_annotations-c42da721367441ec.d
+++
@@ -1,7 +0,0 @@
-/Users/harrynewton/Desktop/Projects/crust_of_rust/lifetime_annotations/target/debug/deps/liblifetime_annotations-c42da721367441ec.rmeta: src/lib.rs
-
-/Users/harrynewton/Desktop/Projects/crust_of_rust/lifetime_annotations/target/debug/deps/liblifetime_annotations-c42da721367441ec.rlib: src/lib.rs
-
-/Users/harrynewton/Desktop/Projects/crust_of_rust/lifetime_annotations/target/debug/deps/lifetime_annotations-c42da721367441ec.d: src/lib.rs
-
-src/lib.rs:
